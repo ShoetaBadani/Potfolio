c359fbd9a43bc4d9fb0d529d73f39bb5bbdacdbf
ShoetaBadani <143995573+ShoetaBadani@users.noreply.github.com>
2023-11-01 16:54:29 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { HeroSection } from './components/HeroSection';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <HeroSection/>
+      
+    </div>
+  );
+}
+
+export default App;
